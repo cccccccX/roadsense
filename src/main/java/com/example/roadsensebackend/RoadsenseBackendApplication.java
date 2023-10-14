183ea1256b576289d8a05eadc6e78508048798c5
徐超超 <787484470@qq.com>
2023-10-14 18:32:02 +0800

    test

---
+++ src/main/java/com/example/roadsensebackend/RoadsenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.roadsensebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadsenseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadsenseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roadsensebackend/RoadsenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadsensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadsenseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

fa260e78f21e3289bcbc26aee9e82eccbd8b3619
Edith <Edith@learn.cn>
2023-10-30 00:15:07 +0800

    测试拦截器

---
+++ src/main/java/com/roadsense/Application.java
@@ -0,0 +1,26 @@
+package com.roadsense;
+
+import com.roadsense.controller.interceptor.LoginInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@MapperScan(basePackages = "com.roadsense.mapper")
+@SpringBootApplication
+public class Application extends WebMvcConfigurationSupport {
+
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/users");
+    }
+}
---
+++ src/main/java/com/roadsense/controller/interceptor/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.roadsense.controller.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * author  Edith
+ * created  2023/10/30 0:05
+ */
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle ...");
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}

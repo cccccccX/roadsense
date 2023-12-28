bdbcd7c375f435fcfaa5cc89c98453245a3cf70d
Edith <Edith@learn.cn>
2023-12-28 18:46:39 +0800

    完成第一阶段开发

--- src/main/java/com/roadsense/Application.java
+++ src/main/java/com/roadsense/Application.java
@@ -12,11 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @SpringBootApplication
 @EnableTransactionManagement
 public class Application{
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
 }
